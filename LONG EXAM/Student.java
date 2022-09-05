af8ed21075c8fad7f4c9261d3ddc34c3388affb4
laguardia69awit <111828535+laguardia69awit@users.noreply.github.com>
2022-09-05 22:39:59 +0800

    Create Student.java

---
+++ LONG EXAM/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    
+    //Properties
+
+String surname;
+String firstName;
+char middleInitial;
+String dateOfBirth;
+int studentNumber;
+String studentEmailAddress;
+boolean iAmAwesome;
+
+    
+    public void sayMystudentNumber() {
+        System.out.println(studentNumber);
+
+    }
+
+    public void sayMyemailAddress() {
+        System.out.println(studentEmailAddress);
+
+    }
+
+    public void sayiAmAwesome() {
+        System.out.println(iAmAwesome);
+
+    }
+
+}
